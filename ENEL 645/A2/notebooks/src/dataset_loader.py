2c9f871437816632324c4fec22f613443c1dfe15
chvaldez10 <christianvaldez021@gmail.com>
2024-02-25 19:10:33 -0700

    modified split_data to return dict

--- ENEL 645/A2/notebooks/src/dataset_loader.py
+++ ENEL 645/A2/notebooks/src/dataset_loader.py
@@ -36,29 +36,7 @@ def list_data_and_prepare_labels(images_path: str) -> tuple:
     labels_int = convert_labels_to_int(labels, classes)
     return images, labels_int, classes
 
-def split_data(images: np.ndarray, labels: np.ndarray, val_split: float, test_split: float) -> tuple:
-    """
-    Split data into train, validation, and test sets.
-    """
-    # Splitting data into test and development sets
-    sss = StratifiedShuffleSplit(n_splits=1, test_size=test_split, random_state=10)
-    dev_index, test_index = next(sss.split(images, labels))
-    dev_images, dev_labels = images[dev_index], labels[dev_index]
-    test_images, test_labels = images[test_index], labels[test_index]
-
-    # Splitting development set into train and validation sets
-    val_size = int(val_split * len(images))
-    val_split_adjusted = val_size / len(dev_images)
-    sss2 = StratifiedShuffleSplit(n_splits=1, test_size=val_split_adjusted, random_state=10)
-    train_index, val_index = next(sss2.split(dev_images, dev_labels))
-
-    # Returning the split data
-    train_images, train_labels = dev_images[train_index], dev_labels[train_index]
-    val_images, val_labels = dev_images[val_index], dev_labels[val_index]
-
-    return train_images, train_labels, val_images, val_labels, test_images, test_labels
-
-def split_data_to_dicts(images: np.ndarray, labels: np.ndarray, val_split: float, test_split: float, random_state: int = 10) -> dict:
+def split_data(images: np.ndarray, labels: np.ndarray, val_split: float, test_split: float, random_state: int = 10) -> tuple:
     """
     Split data into train, validation, and test sets and return them as dictionaries.
     """
@@ -84,22 +62,7 @@ def split_data_to_dicts(images: np.ndarray, labels: np.ndarray, val_split: float
     val_set = {"X": val_images, "Y": val_labels}
     test_set = {"X": test_images, "Y": test_labels}
 
-    return {"train": train_set, "val": val_set, "test": test_set}
-
-# def apply_transforms(mean_train=None, std_train=None):
-#     """
-#     Apply transformations to the datasets.
-#     """
-#     base_transform = [transforms.Resize((224, 224)), transforms.ToTensor()]
-#     if mean_train is not None and std_train is not None:
-#         normalize_transform = transforms.Normalize(mean=mean_train, std=std_train)
-#         train_transform = transforms.Compose(base_transform + [transforms.RandomHorizontalFlip(), transforms.RandomVerticalFlip(), normalize_transform])
-#         test_transform = transforms.Compose(base_transform + [normalize_transform])
-#     else:
-#         train_transform = transforms.Compose(base_transform + [transforms.RandomHorizontalFlip(), transforms.RandomVerticalFlip()])
-#         test_transform = transforms.Compose(base_transform)
-
-#     return train_transform, test_transform
+    return {"Train": train_set, "Validation": val_set, "test": test_set}
 
 # def create_data_loaders(train_set, val_set, test_set, batch_size, train_transform, test_transform):
 #     """
